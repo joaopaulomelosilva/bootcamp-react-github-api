656c48be72cc55f7e35a50875def7daf8810c826
João Paulo de Melo Silva <33991065+joaopaulomelosilva@users.noreply.github.com>
2023-01-08 00:08:23 -0300

    API Request

--- bootcamp-react-github-api/src/pages/Search/index.tsx
+++ bootcamp-react-github-api/src/pages/Search/index.tsx
@@ -1,22 +1,79 @@
-
+import axios from 'axios';
 import ResultCard from './ResultCard';
+import { useState } from 'react';
+import SearchInfoLoader from './SearchInfoLoader';
+
 import './styles.css';
-import foto from '../../assets/img/foto1.png';
+
+
+type Username = {
+    login: string;
+}
+
+type Profile = {
+        avatar_url: string;
+        url: string;
+        location: string;
+        name: string;
+        followers: string;
+}
+
 
 function Search(){
+
+    const [profile, setProfile] = useState<Profile>();
+
+    const [username, setUsername] = useState<Username>({
+        login: '',
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setUsername({...username, [name]: value});
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+        axios.get(`https://api.github.com/users/${username?.login}`)
+        .then((response) => {
+            setProfile(response.data);
+        }).catch((error) => {
+            setProfile(undefined);
+        } ).finally(() => {
+            setIsLoading(false);
+        })
+    }
+
     return(
         <div className='search-container'>
             <div className='search-content'>
                 <h2>Encontre um perfil Github</h2>
-                <form action="" className='form-content'>
-                    <input type="text" placeholder='Usuário Github' />
-                    <button>
+                <form onSubmit={handleSubmit} className='form-content'>
+                    <input type="text" 
+                    name='login' 
+                    value={username.login}
+                    placeholder='Usuário Github'
+                    className='input-text-content'
+                    onChange={handleChange}/>
+
+                    <button type='submit'>
                         <h5>Encontrar</h5>
                     </button>
                 </form>
             </div>
-            <ResultCard image={foto} profile='www.google.com' 
-            followers='4500' Location='Alpinópolis' Name='João Paulo'></ResultCard>
+            {isLoading ? <SearchInfoLoader></SearchInfoLoader> : (profile &&
+                <>
+                <ResultCard image={profile.avatar_url} profile={profile.url} 
+                followers={profile.followers} Location={profile.location} Name={profile.name}></ResultCard>
+                </>
+                )
+            }
         </div>
     )
 }
